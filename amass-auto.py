bc41f729b74131d76ad6225f93bc3fad8b6b7bc4
muhsinbinirshad <muhsinhaseeb@gmail.com>
2023-03-26 13:49:08 +0530

    amass-auto

---
+++ amass-auto.py
@@ -0,0 +1,39 @@
+import os
+
+# Print menu of available Amass functions
+print("Select an Amass function to run:")
+print("1. Enumeration")
+print("2. Passive DNS")
+print("3. Active DNS")
+print("4. Brute Forcing")
+print("5. All of the above")
+
+# Get user's choice of function
+choice = int(input("Enter a number: "))
+
+# Ask user to input domain and unique name for output file
+domain = input("Enter domain to scan: ")
+output_name = input("Enter a unique name for the output file: ")
+
+# Run selected Amass function and output results to file with unique name
+if choice == 1:
+    print("Running Amass Enumeration...")
+    os.system(f"amass enum -d {domain} -o {output_name}_enum.txt")
+elif choice == 2:
+    print("Running Amass Passive DNS...")
+    os.system(f"amass dns -d {domain} -passive -o {output_name}_passive_dns.txt")
+elif choice == 3:
+    print("Running Amass Active DNS...")
+    os.system(f"amass dns -d {domain} -active -o {output_name}_active_dns.txt")
+elif choice == 4:
+    print("Running Amass Brute Forcing...")
+    os.system(f"amass brute -d {domain} -o {output_name}_brute_force.txt")
+elif choice == 5:
+    print("Running all Amass functions...")
+    os.system(f"amass enum -d {domain} -o {output_name}_enum.txt")
+    os.system(f"amass dns -d {domain} -passive -o {output_name}_passive_dns.txt")
+    os.system(f"amass dns -d {domain} -active -o {output_name}_active_dns.txt")
+    os.system(f"amass brute -d {domain} -o {output_name}_brute_force.txt")
+else:
+    print("Invalid choice. Please enter a number from 1 to 5.")
+
